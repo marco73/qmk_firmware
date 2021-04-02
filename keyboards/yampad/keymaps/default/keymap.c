7908bd485c92ab21af03c7663f0fdbaf67830881
marco73 <wauwwie8@hotmail.com>
2021-04-02 22:38:19 +0200

    .

--- keyboards/yampad/keymaps/default/keymap.c
+++ keyboards/yampad/keymaps/default/keymap.c
@@ -69,9 +69,9 @@ void oled_task_user(void) {
 
     // Host Keyboard LED Status
     led_t led_state = host_keyboard_led_state();
-    oled_set_cursor(0, 0);
+    oled_set_cursor(0, 40);
     oled_write_ln(led_state.num_lock ? PSTR("NUM  \b") : PSTR("    "), false);
-    oled_set_cursor(0, 2);
+    oled_set_cursor(4, 10);
     oled_write_ln(led_state.caps_lock ? PSTR("CAP  \b") : PSTR("    "), false);
 }
 #endif
