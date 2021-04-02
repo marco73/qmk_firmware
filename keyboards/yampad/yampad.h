202e3aae8f9f0655ee7aa1f1f7b7739fdc584eaa
marco73 <wauwwie8@hotmail.com>
2021-04-02 17:33:06 +0200

    .

--- keyboards/yampad/yampad.h
+++ keyboards/yampad/yampad.h
@@ -20,29 +20,29 @@
 #include "quantum.h"
 
 #define LAYOUT_base( \
-    KC_NUMLOCK,  KC_KP_SLASH,  KC_KP_ASTERISK,  KC_KP_MINUS, \
-    KC_P7,       KC_P8,        KC_P9,           KC_KP_PLUS,  \
-    KC_P4,       KC_P5,        KC_P6, \
-    KC_P1,       KC_P2,        KC_P3, \
-    TG(_RGB), KC_KP_0,      KC_KP_DOT,       KC_KP_ENTER \
+    KC_NLCK,   KC_PSLS,  KC_PAST,  KC_PMNS, \
+    KC_P7,     KC_P8,    KC_P9,    KC_PPLS,  \
+    KC_P4,     KC_P5,    KC_P6, \
+    KC_P1,     KC_P2,    KC_P3, \
+    TG(_RGB),  KC_P0,    KC_PDOT,  KC_PENT \
 ) { \
-    { KC_NUMLOCK,  KC_KP_SLASH,  KC_KP_ASTERISK,  KC_KP_MINUS, }, \
-    { KC_P7,       KC_P8,        KC_P9,           KC_KP_PLUS, }, \
-    { KC_P4,       KC_P5,        KC_P6,           KC_NO }, \
-    { KC_P1,       KC_P2,        KC_P3,           KC_NO }, \
-    {TG(_RGB), KC_KP_0,      KC_KP_DOT,       KC_KP_ENTER }  \
+    { KC_NLCK,  KC_PSLS,  KC_PAST,  KC_PMNS }, \
+    { KC_P7,    KC_P8,    KC_P9,    KC_PPLS }, \
+    { KC_P4,    KC_P5,    KC_P6,    KC_NO }, \
+    { KC_P1,    KC_P2,    KC_P3,    KC_NO }, \
+    {TG(_RGB),  KC_P0,    KC_PDOT,  KC_PENT },  \
 }
 
 #define LAYOUT_rgb( \
-    KC_NUMLOCK,         ,  RGB_MODE_PLAIN,     RGB_MODE_REVERSE, \
-    RGB_HUD,     RGB_HUI,  RGB_MODE_BREATHE,   RGB_MODE_FORWARD,  \
-    RGB_SAD,     RGB_SAI,  RGB_MODE_SWIRL, \
-    RGB_VAD,     RGB_VAI,  RGB_MODE_SNAKE, \
-    TG(_BASE),          ,  RGB_MODE_GRADIENT,  RGB_TOG \
+    KC_NLCK,          ,   RGB_MODE_PLAIN,     RGB_MODE_REVERSE, \
+    RGB_HUD,    RGB_HUI,  RGB_MODE_BREATHE,   RGB_MODE_FORWARD,  \
+    RGB_SAD,    RGB_SAI,  RGB_MODE_SWIRL, \
+    RGB_VAD,    RGB_VAI,  RGB_MODE_SNAKE, \
+    TG(_BASE),  KC_NO,    RGB_MODE_GRADIENT,  RGB_TOG \
 ) { \
-    { KC_NUMLOCK,  KC_NO,    RGB_MODE_PLAIN,    RGB_MODE_REVERSE, }, \
-    { RGB_HUD,     RGB_HUI,  RGB_MODE_BREATHE,  RGB_MODE_FORWARD, }, \
-    { RGB_SAD,     RGB_SAI,  RGB_MODE_SWIRL,    KC_NO }, \
-    { RGB_VAD,     RGB_VAI,  RGB_MODE_SNAKE,    KC_NO }, \
-    { TG(_BASE),   KC_NO,    RGB_MODE_GRADIENT, RGB_TOG }  \
+    { KC_NLCK,    KC_NO,    RGB_MODE_PLAIN,    RGB_MODE_REVERSE }, \
+    { RGB_HUD,    RGB_HUI,  RGB_MODE_BREATHE,  RGB_MODE_FORWARD }, \
+    { RGB_SAD,    RGB_SAI,  RGB_MODE_SWIRL,    KC_NO }, \
+    { RGB_VAD,    RGB_VAI,  RGB_MODE_SNAKE,    KC_NO }, \
+    { TG(_BASE),  KC_NO,    RGB_MODE_GRADIENT, RGB_TOG },  \
 }
