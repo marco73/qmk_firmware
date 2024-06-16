19097e3d4e47fdc2504b8dccb9076fdb00ccc17b
Marco <wauwwie8@hotmail.com>
2024-06-16 09:13:25 +0200

    Adding my config for Sofle_choc

---
+++ keyboards/sofle_choc/keymaps/MySofleChoc/config.h
@@ -0,0 +1,53 @@
+/* Copyright 2023 Brian Low
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+// Enabling this option changes the startup behavior to listen for an
+// active USB communication to delegate which part is master and which
+// is slave. With this option enabled and theres’s USB communication,
+// then that half assumes it is the master, otherwise it assumes it
+// is the slave.
+//
+// I've found this helps with some ProMicros where the slave does not boot
+#define SPLIT_USB_DETECT
+
+#define RGB_DISABLE_WHEN_USB_SUSPENDED     // turn off effects when suspended
+#define SPLIT_TRANSPORT_MIRROR             // If LED_MATRIX_KEYPRESSES or LED_MATRIX_KEYRELEASES is enabled, you also will want to enable SPLIT_TRANSPORT_MIRROR
+#define SPLIT_WPM_ENABLE                   // Enable spaceship wpm speed on right keyboard also
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100  // limits maximum brightness of LEDs (max 255). Higher may cause the controller to crash.
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #define RGBLIGHT_EFFECT_SNAKE
+    #define RGBLIGHT_EFFECT_KNIGHT
+    #define RGBLIGHT_EFFECT_CHRISTMAS
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//  #define RGBLIGHT_EFFECT_RGB_TEST
+//  #define RGBLIGHT_EFFECT_ALTERNATING
+//  #define RGBLIGHT_EFFECT_TWINKLE
+//  #define RGBLIGHT_LIMIT_VAL 120
+//  #define RGBLIGHT_HUE_STEP 10
+//  #define RGBLIGHT_SAT_STEP 17
+//  #define RGBLIGHT_VAL_STEP 17
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
---
+++ keyboards/sofle_choc/keymaps/MySofleChoc/rules.mk
@@ -0,0 +1,9 @@
+#ENCODER_MAP_ENABLE = yes
+ENCODER_ENABLE 	= yes
+OLED_ENABLE 	= yes
+LTO_ENABLE      = yes
+WPM_ENABLE		= yes
+NKRO_ENABLE     = yes
+TAP_DANCE_ENABLE= yes
+# Bootloader selection
+BOOTLOADER 		= rp2040
